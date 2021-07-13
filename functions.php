9abeefccf473f21c9965ee935a5733b4c3c41c1c
Ermal Vrapi <ermal.vrapi@crispybacon.al>
2021-07-13 08:47:22 +0200

    fix

--- functions.php
+++ functions.php
@@ -322,57 +322,7 @@ add_action('wp_ajax_filter_posts', 'filter_posts_template');
 add_action('wp_ajax_nopriv_filter_posts', 'filter_posts_template');
 
 
-if( function_exists('acf_add_local_field_group') ):
 
-	acf_add_local_field_group(array(
-		'key' => 'group_60c9f2328988d',
-		'title' => 'Category Options',
-		'fields' => array(
-			array(
-				'key' => 'field_60c9f24923514',
-				'label' => 'Category Image',
-				'name' => 'mokilla_category_image',
-				'type' => 'image',
-				'instructions' => '',
-				'required' => 0,
-				'conditional_logic' => 0,
-				'wrapper' => array(
-					'width' => '',
-					'class' => '',
-					'id' => '',
-				),
-				'return_format' => 'url',
-				'preview_size' => 'medium',
-				'library' => 'all',
-				'min_width' => '',
-				'min_height' => '',
-				'min_size' => '',
-				'max_width' => '',
-				'max_height' => '',
-				'max_size' => '',
-				'mime_types' => '',
-			),
-		),
-		'location' => array(
-			array(
-				array(
-					'param' => 'taxonomy',
-					'operator' => '==',
-					'value' => 'category',
-				),
-			),
-		),
-		'menu_order' => 0,
-		'position' => 'normal',
-		'style' => 'default',
-		'label_placement' => 'top',
-		'instruction_placement' => 'label',
-		'hide_on_screen' => '',
-		'active' => true,
-		'description' => '',
-	));
-	
-	endif;
 
 
 /**
