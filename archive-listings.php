be841654def3ae3de869ae00035d4dff58d608a5
Ermal Vrapi <ermalvrapi18@gmail.com>
2021-07-13 20:55:23 +0200

    fix

--- archive-listings.php
+++ archive-listings.php
@@ -11,30 +11,44 @@ get_header();
 ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+		<main id="main" class="site-main listing-container">
 
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-			
+				<p><span>FRONT PAGE</span> >> LISTING PAGE</p>
+				<h1>LISTING PAGE</h1>
 			</header><!-- .page-header -->
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-				get_template_part( 'template-parts/content', get_post_type() );
+			<nav class="filter-nav">
+			filter nav
+			</nav>
 
-			endwhile;
+			<div class="row">
+			
+				<div class="col side">
+					side
+				</div>
+				<div class="col grid content">
+				<?php
+					while ( have_posts() ) :
+						the_post();
+						get_template_part( 'template-parts/content', get_post_type() );
 
-			the_posts_navigation();
+					endwhile;
 
-		else :
+					the_posts_navigation();
 
-			get_template_part( 'template-parts/content', 'none' );
+				else :
 
-		endif;
-		?>
+					get_template_part( 'template-parts/content', 'none' );
+
+				endif;
+				?>
+				</div>
+			
+			</div>
+			
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
--- template-parts/content-listings.php
+++ template-parts/content-listings.php
@@ -1,1 +1,47 @@
-hello
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Mokilla
+ */
+
+use mokilla\mokilla_core_admin\PostTypeListings;
+
+?>
+
+<a href="<?php the_permalink(); ?>" class="content-listing" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	
+   <img src="<?php the_post_thumbnail_url(); ?>" alt="">
+
+    <div class="content">
+        <h3 class="title"><?php the_title(); ?></h3>
+
+        <div class="meta">
+            <?php if(get_field(PostTypeListings::ACF_EMAIL)):  ?>
+                <p><?= get_field(PostTypeListings::ACF_EMAIL) ?></p>
+            <?php endif; ?>
+            
+            <?php if(get_field(PostTypeListings::ACF_PHONE_NUMBER)):  ?>
+                <p><?= get_field(PostTypeListings::ACF_PHONE_NUMBER) ?></p>
+            <?php endif; ?>
+
+            <?php if(get_field(PostTypeListings::ACF_ADDRESS)):  ?>
+                <p><?= get_field(PostTypeListings::ACF_ADDRESS) ?></p>
+            <?php endif; ?>
+
+            <?php if(get_field(PostTypeListings::ACF_WEBSITE)):  ?>
+                <p><?= get_field(PostTypeListings::ACF_WEBSITE) ?></p>
+            <?php endif; ?>
+        </div>
+
+        <footer class="meta">
+        <?php if(get_field(PostTypeListings::ACF_PRICE)):  ?>
+                <p><span>$<?= get_field(PostTypeListings::ACF_PRICE) ?></span> / per order</p>
+            <?php endif; ?>
+        </footer>
+    </div>
+
+
+</a><!-- #post-<?php the_ID(); ?> -->
